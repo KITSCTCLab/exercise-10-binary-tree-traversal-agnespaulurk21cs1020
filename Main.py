aa41477493f87c6347a62dba3c229a6c534e1781
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-10-11 20:41:42 +0530

    final commit

--- Main.py
+++ Main.py
@@ -9,21 +9,21 @@ def insert(root, new_value) -> BinaryTreeNode:
     """If binary search tree is empty, make a new node, declare it as root and return the root.
         If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root."""
+        Finally, return the root.
+        """
     if not root:
         root = BinaryTreeNode(new_value)
         return root
-        if new_value < root.data:
-            if root.left_child:
-                insert(root.left_child, new_value)
-            else:
-                root.left_child = BinaryTreeNode(new_value)
+    if new_value < root.data:
+         if root.left_child:
+            insert(root.left_child, new_value)
+         else:
+            root.left_child = BinaryTreeNode(new_value)
+    else:
+        if root.right_child:
+            insert(root.right_child, new_value)
         else:
-            if root.right_child:
-                insert(root.right_child, new_value)
-            else:
-                root.right_child = BinaryTreeNode(new_value)  
- 
+            root.right_child = BinaryTreeNode(new_value)
 
 
 def inorder(root) -> None:
